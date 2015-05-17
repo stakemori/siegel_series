355c05c217cda96bd40732d00d953923bcde4c4d
Sho Takemori <stakemorii@gmail.com>
2015-05-17 11:24:15 +0900

    Modified jordan_blocks_2.

--- siegel_series.py
+++ siegel_series.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8; mode: sage -*-
 from sage.all import (cached_function, QuadraticForm, ZZ,
                       least_quadratic_nonresidue, legendre_symbol, is_odd)
-
+from degree2.utils import list_group_by
 
 @cached_function
 def does_2adically_rep_zero(a, b, c):
@@ -28,20 +28,22 @@ def _trans_jordan_dec_2(us):
     x1^2 + x2^2 - x3^2 ~ x1^2 + 2*x1*x2.
     x1^2 + x2^2 + x3^2 ~ 3*x1^2 + 2(x2^2 + x2*x3 + x3^2).
     '''
+    hyp2_name = "h"
+    y2_name = "y"
     if len(us) <= 2:
         return us
     u1, u2, u3 = us[:3]
     u = u1*u2*u3
     l = _trans_jordan_dec_2(us[3:])
     if does_2adically_rep_zero(u1, u2, u3):
-        l.extend([(-u) % 8, "h"])
+        l.extend([(-u) % 8, hyp2_name])
     else:
-        l.extend([(3*u)%8, "y"])
-    l_diag = [a for a in l if not a in ["h", "y"]]
+        l.extend([(3*u)%8, y2_name])
+    l_diag = [a for a in l if not a in [hyp2_name, y2_name]]
     if len(l_diag) <= 2:
         return l
     else:
-        l_non_diag = [a for a in l if a in ["h", "y"]]
+        l_non_diag = [a for a in l if a in [hyp2_name, y2_name]]
         l1 = _trans_jordan_dec_2(l_diag)
         return l1 + l_non_diag
 
@@ -81,21 +83,33 @@ def jordan_blocks_2(q):
     If `b' is in [1, 3, 5, 7], then the corresponding
     gram matrix is equal to matrix([[b]]).
     '''
-    res = []
+    ls = []
     for a, q1 in q.jordan_blocks_by_scale_and_unimodular(2):
         m = q1.Gram_matrix()
         while not m.is_zero():
             m00 = m[0, 0]
             if is_odd(m00):
-                res.append((a, m00 % 8))
+                ls.append((a, m00 % 8))
                 m = m.submatrix(1, 1)
             elif m00 == 0:
-                res.append((a + 1, "h"))
+                ls.append((a + 1, "h"))
                 m = m.submatrix(2, 2)
             else:
-                res.append((a + 1, "y"))
+                ls.append((a + 1, "y"))
                 m = m.submatrix(2, 2)
-    return list(reversed(res))
+    # ls is a list of jordan blocks but it may contain diagonal entries of
+    # units of length >= 3.
+    # We take another jordan blocks by _trans_jordan_dec_2.
+    res = [(a, b) for a, b in ls if isinstance(b, str)]
+    diag_elts = [(a, b) for a, b in ls if not isinstance(b, str)]
+    for a, us_w_idx in list_group_by(diag_elts, lambda x: x[0]):
+        l = _trans_jordan_dec_2([_b for _, _b in us_w_idx])
+        for b in l:
+            if isinstance(b, str):
+                res.append((a + 1, b))
+            else:
+                res.append((a, b))
+    return sorted(res, key=lambda x: -x[0])
 
 
 def siegel_series_polynomial(B, p):
