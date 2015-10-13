d646050930da6fd5cf4f6cfb4af6f19929ca3711
Sho Takemori <stakemorii@gmail.com>
2015-10-13 22:58:21 +0900

    Add method fourier_coefficient.

--- siegel_eisenstein.py
+++ siegel_eisenstein.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8; mode: sage -*-
-from sage.all import ZZ, prime_factors, zeta, quadratic_L_function__exact
+from sage.all import ZZ, prime_factors, zeta, quadratic_L_function__exact, QQ, MatrixSpace
 from siegel_series.impl import siegel_series_polynomial, X
 import operator
 
+
 class SiegelEisensteinSeries(object):
+
     '''
     A class for Siegel Eisenstein series of general degrees.
     '''
+
     def __init__(self, weight=None, degree=None):
         self._weight = weight
         self._degree = degree
@@ -24,26 +27,48 @@ class SiegelEisensteinSeries(object):
         '''
         Assuming mat is a positive definite, half-integral matrix,
         it returns the Fourier coefficient of self at the matrix.
+        cf. S. Takemori, Siegel Eisenstein series of degree n and Lambda-adic Eisenstein series.
         '''
         n = self.degree
         k = self.weight
         if n % 2 == 0:
-            _mat_det = mat.det() * ZZ(2)**n
+            _mat_det = ZZ(mat.det() * ZZ(2) ** n)
         else:
-            _mat_det = mat.det() * ZZ(2)**(n - 1)
+            _mat_det = ZZ(mat.det() * ZZ(2) ** (n - 1))
         unramfac = reduce(operator.mul,
                           (siegel_series_polynomial(mat, p).subs(
-                              {X: p**(n + 1 - k)})
-                           for p in prime_factors(_mat_det)))
+                              {X: p ** (k - n - 1)})
+                           for p in prime_factors(_mat_det)), ZZ(1))
         if n % 2 == 0:
             quadl = quadratic_L_function__exact(
-                1 + n//2 - k, (-1)**(n//2) * mat.det)
+                1 + n // 2 - k, (-1) ** (n // 2) * _mat_det)
             prod = reduce(operator.mul,
-                          (zeta(1 + 2*i - 2*k)**(-1)
-                           for i in range(1, n//2 + 1)), ZZ(1))
-            return ZZ(2)**(n//2) * zeta(1 - k)**(-1) * quadl * prod * unramfac
+                          (zeta(1 + 2 * i - 2 * k)
+                           for i in range(1, n // 2 + 1)), ZZ(1)) ** (-1)
+            return ZZ(2) ** (n // 2) * zeta(1 - k) ** (-1) * quadl * prod * unramfac
         else:
             prod = reduce(operator.mul,
-                          (zeta(1 + 2*i - 2*k)**(-1)
-                           for i in range(1, (n - 1)//2 + 1)), ZZ(1))
-            return ZZ(2)**((n + 1)//2) * zeta(1 - k)**(-1) * prod * unramfac
+                          (zeta(1 + 2 * i - 2 * k)
+                           for i in range(1, (n - 1) // 2 + 1)), ZZ(1)) ** (-1)
+            return ZZ(2) ** ((n + 1) // 2) * zeta(1 - k) ** (-1) * prod * unramfac
+
+    def fourier_coefficient(self, mat, check=False):
+        '''Assuming mat is a half-integral, semi positive definite matrix,
+        returns the Fourier coefficient at mat.
+        If check is True, then it checks mat is semi-positive definite.
+        '''
+        if mat == 0:
+            return QQ(1)
+        n = mat.ncols()
+        if check:
+            if not all(a >= 0 for a in mat.indefinite_factorization(algoritm='symmetric')[1]):
+                return QQ(0)
+        MZ = MatrixSpace(ZZ, n)
+        _, u, _ = MZ(mat * 2, n).smith_form()
+        r = mat.rank()
+        if r == n:
+            return self._fourier_coefficient_full_rank(mat)
+        else:
+            mat_rk_r = (u * mat * u.transpose()).submatrix(ncols=r, nrows=r)
+            eisen_deg_r = SiegelEisensteinSeries(weight=self.weight, degree=r)
+            return eisen_deg_r._fourier_coefficient_full_rank(mat_rk_r)
