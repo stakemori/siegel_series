4be4b96caa2ba919c26bb735d50a2f20aba25a2e
Sho Takemori <stakemorii@gmail.com>
2015-10-15 00:20:49 +0900

    Add method _product_of_l_part.

--- siegel_eisenstein.py
+++ siegel_eisenstein.py
@@ -40,18 +40,23 @@ class SiegelEisensteinSeries(object):
                           (siegel_series_polynomial(mat, p).subs(
                               {X: p ** (k - n - 1)})
                            for p in prime_factors(_mat_det)), ZZ(1))
+        return self._product_of_l_part(k, _mat_det, n) * unramfac
+
+    def _product_of_l_part(self, k, mat_det, n):
+        '''Factor of Fourier coefficient except unramified factors.
+        '''
         if n % 2 == 0:
             quadl = quadratic_L_function__exact(
-                1 + n // 2 - k, (-1) ** (n // 2) * _mat_det)
+                1 + n // 2 - k, (-1) ** (n // 2) * mat_det)
             prod = reduce(operator.mul,
                           (zeta(1 + 2 * i - 2 * k)
                            for i in range(1, n // 2 + 1)), ZZ(1)) ** (-1)
-            return ZZ(2) ** (n // 2) * zeta(1 - k) ** (-1) * quadl * prod * unramfac
+            return ZZ(2) ** (n // 2) * zeta(1 - k) ** (-1) * quadl * prod
         else:
             prod = reduce(operator.mul,
                           (zeta(1 + 2 * i - 2 * k)
                            for i in range(1, (n - 1) // 2 + 1)), ZZ(1)) ** (-1)
-            return ZZ(2) ** ((n + 1) // 2) * zeta(1 - k) ** (-1) * prod * unramfac
+            return ZZ(2) ** ((n + 1) // 2) * zeta(1 - k) ** (-1) * prod
 
     def fourier_coefficient(self, mat):
         '''Assuming mat is a half-integral, semi positive definite matrix,
