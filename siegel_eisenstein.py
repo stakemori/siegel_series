70786cc8cd1840231b48b334b102832b7309438c
Sho Takemori <stakemorii@gmail.com>
2015-10-15 12:31:04 +0900

    minor change.

--- siegel_eisenstein.py
+++ siegel_eisenstein.py
@@ -24,6 +24,14 @@ class SiegelEisensteinSeries(object):
     def degree(self):
         return self._degree
 
+    def _mat_det_full_rank(self, mat):
+        n = self.degree
+        if n % 2 == 0:
+            _mat_det = ZZ(mat.det() * ZZ(2) ** n)
+        else:
+            _mat_det = ZZ(mat.det() * ZZ(2) ** (n - 1))
+        return _mat_det
+
     def _fourier_coefficient_full_rank(self, mat):
         '''
         Assuming mat is a positive definite, half-integral matrix,
@@ -33,19 +41,19 @@ class SiegelEisensteinSeries(object):
         '''
         n = self.degree
         k = self.weight
-        if n % 2 == 0:
-            _mat_det = ZZ(mat.det() * ZZ(2) ** n)
-        else:
-            _mat_det = ZZ(mat.det() * ZZ(2) ** (n - 1))
+        _mat_det = self._mat_det_full_rank(mat)
         unramfac = reduce(operator.mul,
                           (siegel_series_polynomial(mat, p).subs(
                               {X: p ** (k - n - 1)})
                            for p in prime_factors(_mat_det)), ZZ(1))
-        return self._product_of_l_part(k, _mat_det, n) * unramfac
+        return self._product_of_l_part(mat) * unramfac
 
-    def _product_of_l_part(self, k, mat_det, n):
+    def _product_of_l_part(self, mat):
         '''Factor of Fourier coefficient except unramified factors.
         '''
+        mat_det = self._mat_det_full_rank(mat)
+        n = self.degree
+        k = self.weight
         if n % 2 == 0:
             quadl = quadratic_L_function__exact(
                 1 + n // 2 - k, (-1) ** (n // 2) * mat_det)
