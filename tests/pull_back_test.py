52368e808c022021985e005f6ebf816cb95313d8
Sho Takemori <stakemorii@gmail.com>
2015-10-16 22:30:36 +0900

    Add test for pull back.

---
+++ tests/pull_back_test.py
@@ -0,0 +1,49 @@
+from sage.all import ZZ, matrix, diagonal_matrix, eisenstein_series_qexp
+from siegel_series.pull_back_of_siegel_eisen import eisenstein_pull_back_coeff
+from degree2.all import eisenstein_series_degree2
+from degree2.basic_operation import PrecisionDeg2
+import unittest
+
+
+def tpl_to_half_int_mat(t):
+    n, r, m = t
+    return matrix([[ZZ(n), ZZ(r) / ZZ(2)], [ZZ(r) / ZZ(2), ZZ(m)]])
+
+
+class PullBackTest(unittest.TestCase):
+
+    def assert_pull_back_m_1(self, k, A1, prec=10):
+        f0 = eisenstein_pull_back_coeff(k, A1, diagonal_matrix([0]))
+        f = eisenstein_series_qexp(k, prec=prec, normalization='constant') * f0
+        for a in range(prec):
+            self.assertEqual(f[a],
+                             eisenstein_pull_back_coeff(k, A1, diagonal_matrix([a])))
+
+    def assert_pull_back_m_2(self, k, A1, prec=3):
+        f0 = eisenstein_pull_back_coeff(k, A1, tpl_to_half_int_mat((0, 0, 0)))
+        f = eisenstein_series_degree2(k, prec=5) * f0
+        for t in PrecisionDeg2(prec):
+            self.assertEqual(f[t],
+                             eisenstein_pull_back_coeff(k, A1, tpl_to_half_int_mat(t)))
+
+    def test_pull_back_2_1(self):
+        self.assert_pull_back_m_1(6, tpl_to_half_int_mat((4, 0, 6)))
+        self.assert_pull_back_m_1(6, tpl_to_half_int_mat((1, 1, 1)))
+
+    def test_pull_back_3_1(self):
+        self.assert_pull_back_m_1(6, diagonal_matrix([1, 1, 1]))
+        self.assert_pull_back_m_1(6, matrix([[1, 1, 0],
+                                             [1, 1, 0],
+                                             [0, 0, 2]]))
+
+    def test_pull_back_4_1(self):
+        self.assert_pull_back_m_1(8, diagonal_matrix([1, 1, 1, 1]), prec=10)
+        self.assert_pull_back_m_1(8, diagonal_matrix([2, 1, 1, 1]), prec=8)
+
+    def test_pull_back_2_2(self):
+        self.assert_pull_back_m_2(6, tpl_to_half_int_mat((1, 0, 1)))
+        self.assert_pull_back_m_2(6, tpl_to_half_int_mat((1, 1, 1)))
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(PullBackTest)
+unittest.TextTestRunner(verbosity=2).run(suite)
